cac15ce649abf847d34d38769b6a195afe446593
Antonio3veces3 <cramirez28@ucol.mx>
2023-06-05 19:32:47 -0600

    Created method for frame and printgame
    - Created method for create a game
    - Creathed a method for print an entire game
    - Bonus strikes and spares undeveloped
    
    Co-authored-by: BrandonVG <brandon.vadillox3@gmail.com>
    Co-authored-by: Eduardoespl <eduardoesp3317@gmail.com>

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const bowling_1 = __importDefault(require("./bowling"));
+const game = new bowling_1.default();
+game.createAndPrintFrames();
+//# sourceMappingURL=index.js.map
--- test/bowling.test.ts
+++ test/bowling.test.ts
@@ -1,4 +1,4 @@
-import BowlingGame from '../bowling';
+import BowlingGame from '../src/bowling';
 
 describe('Tests for BowlingGame class',() => {
 
@@ -9,7 +9,14 @@ describe('Tests for BowlingGame class',() => {
 
   test('A bowling shot return less or equal to 2',() =>{
     const game = new BowlingGame();
-    game.firstShot = 8;
+    game.currentFrame[0] = 8;
     expect(game.shot()).toBeLessThanOrEqual(2);
   });
+
+  test('Should return the sum of the 2 shots and be less than 10', () =>{
+    const game = new BowlingGame();
+    game.createFrame(0);
+    let sum = game.frames[0][0] + game.frames[0][1];
+    expect(sum).toBeLessThanOrEqual(10);
+  })
 });
